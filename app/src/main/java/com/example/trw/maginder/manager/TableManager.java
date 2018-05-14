a0d880f21b4378cb2b9ccbb94bb60916e0fead1e
TRWxCH4MP <thirawat_th13@hotmail.com>
2018-05-14 21:17:15 +0700

    Refactor code table manager class

--- app/src/main/java/com/example/trw/maginder/manager/TableManager.java
+++ app/src/main/java/com/example/trw/maginder/manager/TableManager.java
@@ -1,7 +1,20 @@
 package com.example.trw.maginder.manager;
 
+import com.example.trw.maginder.adapter.item.BaseItem;
+import com.example.trw.maginder.adapter.item.TableItem;
+import com.example.trw.maginder.create_item.CreateTableItem;
+import com.example.trw.maginder.model.StaticStringHelper;
 import com.example.trw.maginder.service.dao.TableItemCollectionDao;
 import com.example.trw.maginder.service.http_manger.HttpManagerTable;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -11,11 +24,58 @@ public class TableManager {
 
     private static TableManager manager;
 
+    private String zoneId;
+    private String tableId;
+    private String tableState;
+    private String tableName;
+    private String transaction;
+    private List<CreateTableItem> listTable = new ArrayList<>();
+
     public static TableManager getInstance() {
         if (manager == null) {
             manager = new TableManager();
         }
         return manager;
+    }
+
+    public String getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(String zoneId) {
+        this.zoneId = zoneId;
+    }
+
+    public String getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(String tableId) {
+        this.tableId = tableId;
+    }
+
+    public String getTableState() {
+        return tableState;
+    }
+
+    public void setTableState(String tableState) {
+        this.tableState = tableState;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(String transaction) {
+        this.transaction = transaction;
     }
 
     public void onCreateTableZone(String restaurantId, final TableManagerCallback callback) {
@@ -38,9 +98,144 @@ public class TableManager {
         });
     }
 
+    public void onSetupTable(final SetupTableCallback callback) {
+//        Log.d(TAG, "getDataRestaurantTable: " + getZoneId());
+        listTable.clear();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference()
+                .child(StaticStringHelper.REF_FIREBASE_TABLE)
+                .child(AuthManager.getInstance().getCurrentRestaurantId())
+                .child(zoneId);
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                Log.d(TAG, "onChildAdded: " + getZoneId());
+                CreateTableItem data = dataSnapshot.getValue(CreateTableItem.class);
+                listTable.add(data);
+                callback.onTableAdded(listTable);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//                Log.d(TAG, "onChildChanged: " + getZoneId());
+                CreateTableItem data = dataSnapshot.getValue(CreateTableItem.class);
+
+                if (getZoneId().equals(data.getId_zone())) {
+//                    Log.d(TAG, "onChildChanged: zone id it's match");
+                    int index = updateItemOnDataChange(data);
+//                    Log.d(TAG, "updateItemOnDataChange2 index: " + index);
+                    listTable.set(index, data);
+                    callback.onTableChanged(listTable);
+                } else {
+                    callback.onZoneIdIsNotMatch("ZoneId not match !");
+//                    Log.d(TAG, "onChildChanged: zone id not match !");
+                }
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        callback.onSuccess(true);
+    }
+
+    private int updateItemOnDataChange(CreateTableItem data) {
+        int index = -1;
+//        Log.d(TAG, "updateItemOnDataChange2 table name: " + data.getName_table());
+        for (int i = 0; i < listTable.size(); i++) {
+            if (listTable.get(i).getId_table().equals(data.getId_table())) {
+                index = i;
+//                Log.d(TAG, "updateItemOnDataChange2: " + i);
+                break;
+            }
+        }
+        return index;
+    }
+
+    public List<BaseItem> onCreateTable(List<CreateTableItem> listTable) {
+        List<BaseItem> itemList = new ArrayList<>();
+
+        for (int index = 0; index < listTable.size(); index++) {
+            if (listTable.get(index).getName_table() != null && listTable.get(index).getStatus() != null) {
+                itemList.add(new TableItem()
+                        .setTableName(listTable.get(index).getName_table())
+                        .setTableState(listTable.get(index).getStatus())
+                        .setTableId(listTable.get(index).getId_table())
+                        .setZoneId(listTable.get(index).getId_zone())
+                );
+            }
+        }
+
+        return itemList;
+    }
+
+    public void updateTableState(final UpdateTableStateCallback callback) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference =
+                database.getReference()
+                        .child(StaticStringHelper.REF_FIREBASE_TABLE)
+                        .child(AuthManager.getInstance().getCurrentRestaurantId())
+                        .child(zoneId);
+
+        databaseReference.orderByChild(StaticStringHelper.REF_FIREBASE_CHILD_TABLE_ID).equalTo(tableId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String key = dataSnapshot.getChildren().iterator().next().getKey();
+//                                    Log.d(TAG, "onDataChange: " + key);
+                        databaseReference
+                                .child(key)
+                                .child(StaticStringHelper.REF_FIREBASE_CHILD_TABLE_STATUS)
+                                .setValue(StaticStringHelper.TRUE, new DatabaseReference.CompletionListener() {
+                                    @Override
+                                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                        callback.onUpdateSuccess(true);
+                                    }
+                                });
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+
     public interface TableManagerCallback {
         void onCreateSuccess(TableItemCollectionDao dao);
 
         void onCreateFailure(String exception);
+    }
+
+    public interface UpdateTableStateCallback {
+        void onUpdateSuccess(boolean isSuccess);
+    }
+
+    public interface SetupTableCallback {
+        void onTableAdded(List<CreateTableItem> listTable);
+
+        void onTableChanged(List<CreateTableItem> listTable);
+
+        void onSuccess(boolean isSuccess);
+
+        void onZoneIdIsNotMatch(String error);
     }
 }
