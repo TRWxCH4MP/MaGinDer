06117c327aeb9a8cd27b4f18960edaaab62eed38
TRWxCH4MP <thirawat_th13@hotmail.com>
2018-05-14 23:13:23 +0700

    rename menu callback

---
+++ app/src/main/java/com/example/trw/maginder/callback/MenuListenerCallback.java
@@ -0,0 +1,15 @@
+package com.example.trw.maginder.callback;
+
+import com.example.trw.maginder.service.dao.RestaurantItemDao;
+
+import java.util.List;
+
+/**
+ * Created by _TRW on 21/1/2561.
+ */
+
+public interface MenuListenerCallback {
+
+    void menuListenerCallback(boolean isSuccess);
+
+}
