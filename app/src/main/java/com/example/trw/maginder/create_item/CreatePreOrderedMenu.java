5f35b9d40d12423e38ee5b91622224757c9d79c6
TRWxCH4MP <thirawat_th13@hotmail.com>
2018-05-25 20:51:43 +0700

    create create pre ordered menu class

---
+++ app/src/main/java/com/example/trw/maginder/create_item/CreatePreOrderedMenu.java
@@ -0,0 +1,113 @@
+package com.example.trw.maginder.create_item;
+
+public class CreatePreOrderedMenu {
+
+    private String id;
+    private String price;
+    private String id_kitchen;
+    private String status;
+    private String name;
+    private String img;
+    private String transaction_menu;
+    private String id_menu;
+    private String date;
+    private String timestamp;
+    private String name_user;
+    private String transaction_status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getId_kitchen() {
+        return id_kitchen;
+    }
+
+    public void setId_kitchen(String id_kitchen) {
+        this.id_kitchen = id_kitchen;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTransaction_menu() {
+        return transaction_menu;
+    }
+
+    public void setTransaction_menu(String transaction_menu) {
+        this.transaction_menu = transaction_menu;
+    }
+
+    public String getId_menu() {
+        return id_menu;
+    }
+
+    public void setId_menu(String id_menu) {
+        this.id_menu = id_menu;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getName_user() {
+        return name_user;
+    }
+
+    public void setName_user(String name_user) {
+        this.name_user = name_user;
+    }
+
+    public String getTransaction_status() {
+        return transaction_status;
+    }
+
+    public void setTransaction_status(String transaction_status) {
+        this.transaction_status = transaction_status;
+    }
+}
